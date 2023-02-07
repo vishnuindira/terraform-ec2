12624a4871b0a9d88addf0e2a19bafa63f3c99a4
Vishnu prasad <vishnuindira123@gmail.com>
2023-02-07 14:58:30 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -12,11 +12,3 @@ provider "aws" {
   region     = "us-west-1"
 }
 
-resource "aws_instance" "terra-cloud-ec2" {
-  ami                         = "ami-0036b4598ccd42565"
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  tags = {
-    "Name" = "terra-cloud-ec2"
-  }
-}
