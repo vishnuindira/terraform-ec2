065603e7c75783c89831530485f4e3a2b655a474
Vishnu prasad <vishnuindira123@gmail.com>
2023-02-07 14:11:08 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,6 +7,11 @@ terraform {
   }
 }
 
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-west-1"
+}
+
 resource "aws_instance" "terra-cloud-ec2" {
   ami                         = "ami-0036b4598ccd42565"
   instance_type               = "t2.micro"
